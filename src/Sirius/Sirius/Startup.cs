ee25a635f817afe59bb319c4a36c8196aa1b889f
Anna Naumova <horovody@gmail.com>
2018-01-04 13:14:56 +0700

    update angular version to 5.2.0

--- src/Sirius/Sirius/Startup.cs
+++ src/Sirius/Sirius/Startup.cs
@@ -30,51 +30,7 @@ namespace Sirius
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IConfiguration>(Configuration);
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<SiriusDbContext>()
-                .AddDefaultTokenProviders();
-            // Identity options.
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireLowercase = false;
-                // Lockout settings.
-                options.Lockout.AllowedForNewUsers = true;
-                options.Lockout.MaxFailedAccessAttempts = 3;
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(1);
-            });
-            // Role based Authorization: policy based role checks.
-            services.AddAuthorization(options =>
-            {
-                // Policy for dashboard: only administrator role.
-                options.AddPolicy("Manage", policy => policy.RequireRole("admin"));
-                // Policy for resources: user or administrator roles. 
-                options.AddPolicy("Access", policy => policy.RequireRole("admin", "user"));
-            });
-            // Adds IdentityServer.
-            services.AddIdentityServer()
-                // The AddDeveloperSigningCredential extension creates temporary key material for signing tokens.
-                // This might be useful to get started, but needs to be replaced by some persistent key material for production scenarios.
-                // See http://docs.identityserver.io/en/release/topics/crypto.html#refcrypto for more information.
-                .AddDeveloperSigningCredential()
-                .AddInMemoryPersistedGrants()
-                // TODO: configure IdentityServer to use EntityFramework (EF) as the storage mechanism for configuration data
-                // see https://identityserver4.readthedocs.io/en/release/quickstarts/8_entity_framework.html
-                .AddInMemoryIdentityResources(IdentityConfig.GetIdentityResources())
-                .AddInMemoryApiResources(IdentityConfig.GetApiResources())
-                .AddInMemoryClients(IdentityConfig.GetClients())
-                .AddAspNetIdentity<IdentityUser>(); // IdentityServer4.AspNetIdentity.
-            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-                .AddIdentityServerAuthentication(options =>
-                {
-                    options.Authority = "http://localhost:59136";
-                    options.RequireHttpsMetadata = false;
-                    options.ApiName = "WebAPI";
-                });
+            this.AddIdentity(services);
 
             services.AddMvc();
 
@@ -126,6 +82,55 @@ namespace Sirius
             }
 
             appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
+        }
+
+        private void AddIdentity(IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<SiriusDbContext>()
+                .AddDefaultTokenProviders();
+            // Identity options.
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                // Lockout settings.
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(1);
+            });
+            // Role based Authorization: policy based role checks.
+            services.AddAuthorization(options =>
+            {
+                // Policy for dashboard: only administrator role.
+                options.AddPolicy("Manage", policy => policy.RequireRole("admin"));
+                // Policy for resources: user or administrator roles. 
+                options.AddPolicy("Access", policy => policy.RequireRole("admin", "user"));
+            });
+            // Adds IdentityServer.
+            services.AddIdentityServer()
+                // The AddDeveloperSigningCredential extension creates temporary key material for signing tokens.
+                // This might be useful to get started, but needs to be replaced by some persistent key material for production scenarios.
+                // See http://docs.identityserver.io/en/release/topics/crypto.html#refcrypto for more information.
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                // TODO: configure IdentityServer to use EntityFramework (EF) as the storage mechanism for configuration data
+                // see https://identityserver4.readthedocs.io/en/release/quickstarts/8_entity_framework.html
+                .AddInMemoryIdentityResources(IdentityConfig.GetIdentityResources())
+                .AddInMemoryApiResources(IdentityConfig.GetApiResources())
+                .AddInMemoryClients(IdentityConfig.GetClients())
+                .AddAspNetIdentity<IdentityUser>(); // IdentityServer4.AspNetIdentity.
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:59136";//todo: move to config
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "WebAPI";
+                });
         }
     }
 }
